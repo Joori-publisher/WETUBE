82be74b3cc9198195ec60cacc6a01e881b8f968b
jooripark <wnfl2828@gmail.com>
2020-07-06 09:35:08 +0900

    #3.1 Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,35 +1,16 @@
-export const videos =[
+import mongooese from "mongoose";
+
+mongooese.connect("mongodb://localhost:27017/we-tube",
     {
-        id:123,
-        title:'Video111',
-        description:'This is sample111',
-        views:23,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            name:'Joori',
-            email:'jojo@gmail.com'
-        }
-    },
-    {
-        id:345,
-        title:'Video222',
-        description:'This is sample222',
-        views:2,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            name:'Joori2',
-            email:'jojo2@gmail.com'
-        }
-    },
-    {
-        id:567,
-        title:'Video666',
-        description:'This is sample666',
-        views:3,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            name:'Joori3',
-            email:'jojo3@gmail.com'
-        }
+        useNewUrlParser:true,
+        useFindAndModify:false
     }
-]
+);
+
+const db = mongooese.connection;
+
+const hangleOpen = () =>console.log("✅ connected to DB");
+const hangleError = (error) =>console.log(`❌ Error on DB Connection : ${error}`);
+
+db.once("open",hangleOpen);
+db.on("error",hangleError);
