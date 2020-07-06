7f81cf97f5cb716b4c1b3872537737815fdc92c3
jooripark <wnfl2828@gmail.com>
2020-07-06 10:12:28 +0900

    #3.4 Comment Model

--- init.js
+++ init.js
@@ -3,8 +3,9 @@ import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
 import "./models/Video";
+import "./models/Comment";
 
-const PORT = process.env.PORT ||PORT;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`✅ Listening on : http://localhost:${PORT}`);
-app.listen(PORT,handleListening)
+app.listen(PORT,handleListening);
